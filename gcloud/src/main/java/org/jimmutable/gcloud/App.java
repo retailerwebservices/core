16936ccf24515d36d0a6d91ec99eda75f31b72a1
James H. Kane Jr <jim.kane@gmail.com>
2017-08-05 10:21:18 -0700

    Created the gcloud project

---
+++ gcloud/src/main/java/org/jimmutable/gcloud/App.java
@@ -0,0 +1,13 @@
+package org.jimmutable.gcloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
