53c8a1b8ee2729c3ee2265e530fb057a1489056c
Andrew Towe <andrew.towe@digitalpanda.com>
2017-08-31 15:47:55 -0700

    PNDA-155 changes after code review

--- aws/src/main/java/org/jimmutable/storage/StorageKeyExtension.java
+++ aws/src/main/java/org/jimmutable/storage/StorageKeyExtension.java
@@ -1,75 +1,55 @@
 package org.jimmutable.storage;
 
-import org.jimmutable.core.exceptions.ValidationException;
 import org.jimmutable.core.objects.Stringable;
 import org.jimmutable.core.utils.Validator;
 
-public class StorageKeyExtension extends Stringable{
-
-	public static final String APPLICATION = "application/";
-	public static final String TEXT = "text";
-	public static final String IMAGE = "image";
-	public static final String AUDIO = "audio";
-	public static final String VIDEO = "video";
-	public static final String HTML="html";
-	public static final String HTM="htm";
-	public static final String CSS="css";
-	public static final String JS="js";
-	public static final String JSON="json";
-	public static final String XML="xml";
-	public static final String JPEG="jpeg";
-	public static final String JPG="jpg";
-	public static final String GIF="gif";
-	public static final String PNG="png";
-	public static final String PDF="pdf";
-	public static final String XSLX="xslx";
-	public static final String CSV="csv";
-	public static final String TXT="txt";
-	public static final String UNKNOWN="OCTET-STREAM";
-	public StorageKeyExtension(String value){
+public class StorageKeyExtension extends Stringable
+{	
+	public StorageKeyExtension(String value)
+	{
 		super(value);
 	}
+	/**
+	 * @return the mime type of this extension.  
+	 */
 
-	public String getSimpleMimeType() {
+	public String getSimpleMimeType() 
+	{
 		switch(getSimpleValue()){
-		case HTML: return TEXT+HTML;
-		case HTM: return TEXT+HTM;
-		case CSS: return TEXT+CSS;
-		case JS: return APPLICATION+JS;
-		case JSON: return APPLICATION+JSON;
-		case XML: return APPLICATION+XML;
-		case JPEG: return IMAGE+JPEG;
-		case JPG: return IMAGE+JPG;
-		case GIF: return IMAGE+GIF;
-		case PNG: return IMAGE+PNG;
-		case PDF: return APPLICATION+PDF;
-		case XSLX: return APPLICATION+XSLX;
-		case CSV: return TEXT+CSV;
-		case TXT: return TEXT+TXT;
-		default: return APPLICATION+UNKNOWN;
+		case "html": return "text/html";
+		case "htm": return "text/htm";
+		case "css": return "text/css";
+		case "js": return "application/js";
+		case "json": return "application/json";
+		case "xml": return "application/xml";
+		case "jpeg": return "image/jpeg";
+		case "jpg": return "image/jpg";
+		case "gif": return "image/gif";
+		case "png": return "image/png";
+		case "pdf": return "application/pdf";
+		case "xslx": return "application/xslx";
+		case "csv": return "text/csv";
+		case "txt": return "text/txt";
+		default: return "application/octet-stream";
 		}
-
 	}
 	
 	@Override
-	public void normalize() {
+	public void normalize() 
+	{
+		Validator.notNull(getSimpleValue());
+		if(getSimpleValue().contains("."))
+		{//we want to strip out any leading ".". so that we do not confuse ourselves. 
+			setValue(getSimpleValue().substring(1));
+		}
 		normalizeLowerCase();
 	}
 
 	@Override
-	public void validate() {
-		Validator.notNull(getSimpleValue());
-		Validator.min(getSimpleValue().length(), 1);
-		
-		char chars[] = getSimpleValue().toCharArray();
-		for ( char ch : chars )
-		{
-			if ( ch >= 'a' && ch <= 'z' ) continue;
-			if ( ch >= '0' && ch <= '9' ) continue;
-			
-			throw new ValidationException(String.format("Illegal character \'%c\' in item attribute %s.  Only lower case letters and numbers are allowed", ch, getSimpleValue()));
-		}
-		
+	public void validate() 
+	{
+		Validator.min(getSimpleValue().length(), 1);//if we strip out the "." and nothing is left over, it will be caught here. 
+		Validator.containsOnlyValidCharacters(getSimpleValue(),Validator.LOWERCASE_LETTERS,Validator.NUMBERS);		
 	}
 
 }
