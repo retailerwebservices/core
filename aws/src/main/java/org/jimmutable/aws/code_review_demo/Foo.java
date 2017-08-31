dc4efa62e5905d6abf30da3fc16c6a531e5e50d1
James H. Kane Jr <jim.kane@gmail.com>
2017-08-31 13:20:08 -0700

    Ready to review!

--- aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
+++ aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
@@ -2,6 +2,6 @@ package org.jimmutable.aws.code_review_demo;
 
 public class Foo {
 	static public void main(String args[]) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! In my class another changes");
 	}
 }
