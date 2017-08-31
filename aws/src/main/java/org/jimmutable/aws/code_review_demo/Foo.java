98f200b1ba77dc263aa548aef5e8543a558bc1ec
James H. Kane Jr <jim.kane@gmail.com>
2017-08-31 12:56:46 -0700

    Made some further changes!

--- aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
+++ aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
@@ -3,5 +3,6 @@ package org.jimmutable.aws.code_review_demo;
 public class Foo {
 	static public void main(String args[]) {
 		System.out.println("Hello World!");
+		System.out.println("Ok, so I did some more work!");
 	}
 }
