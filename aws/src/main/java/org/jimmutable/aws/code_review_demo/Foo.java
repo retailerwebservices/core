60311144b29ecaad2e8f7f6d4a11458e657459c2
James H. Kane Jr <jim.kane@gmail.com>
2017-08-31 13:26:51 -0700

    Did a code review on Foo

--- aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
+++ aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
@@ -1,5 +1,11 @@
 package org.jimmutable.aws.code_review_demo;
 
+/** 
+ * 
+ * CODE REVIEW: Hey --  you have your open braces on the same line, when they should be on the next line per our standards.
+ * @author kanej
+ *
+ */
 public class Foo {
 	static public void main(String args[]) {
 		System.out.println("Hello World! In my class another changes");
