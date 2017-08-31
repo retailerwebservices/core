f85ab7e9f8f2fe0bd22e27195da07f3beee366f3
James H. Kane Jr <jim.kane@gmail.com>
2017-08-31 15:12:53 -0700

    Code review

--- aws/src/main/java/org/jimmutable/aws/servlet_utils/common_objects/JSONServletResponse.java
+++ aws/src/main/java/org/jimmutable/aws/servlet_utils/common_objects/JSONServletResponse.java
@@ -2,6 +2,14 @@ package org.jimmutable.aws.servlet_utils.common_objects;
 
 import org.jimmutable.core.objects.StandardImmutableObject;
 
+/**
+ * CODE REVIEW
+ * 
+ * Needs javadoc comments
+ * @author kanej
+ *
+ */
+
 public abstract class JSONServletResponse extends StandardImmutableObject<JSONServletResponse>
 {
 	public abstract int getSimpleHTTPResponseCode();
