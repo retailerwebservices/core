d11fba481e3a1172670c707cd6df827688ed621f
James H. Kane Jr <jim.kane@gmail.com>
2017-08-31 12:59:53 -0700

    Took foo back to its base state

--- aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
+++ aws/src/main/java/org/jimmutable/aws/code_review_demo/Foo.java
@@ -3,6 +3,5 @@ package org.jimmutable.aws.code_review_demo;
 public class Foo {
 	static public void main(String args[]) {
 		System.out.println("Hello World!");
-		System.out.println("Ok, so I did some more work!");
 	}
 }
