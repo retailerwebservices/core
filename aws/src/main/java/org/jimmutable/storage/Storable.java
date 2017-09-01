b9f763b46e0b66d7f5f7da559a07e72aa138ae4d
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-01 11:22:14 -0700

    PNDA-* after code review commit.

--- aws/src/main/java/org/jimmutable/storage/Storable.java
+++ aws/src/main/java/org/jimmutable/storage/Storable.java
@@ -1,28 +1,47 @@
 package org.jimmutable.storage;
 
-
 import org.jimmutable.core.objects.common.Kind;
 import org.jimmutable.core.objects.common.ObjectId;
 import org.jimmutable.core.serialization.Format;
+
 /**
  *
- * @author andrew.towe
- * This class is designed to help us with object storage. All things that can be stored will be a child of this class.
+ * @author andrew.towe This class is designed to help us with object storage.
+ *         All things that can be stored will be a child of this class.
  *
  */
 
 public abstract class Storable
 {
-	// CODE REVIEW: These methods need javadoc
-	
+
+	/**
+	 * @return The Kind for the Storable Object
+	 */
 	abstract public Kind getSimpleKind();
+
+	/**
+	 * 
+	 * @return The ObjectId for the Storable Object
+	 */
 	abstract public ObjectId getSimpleObjectId();
-	abstract public String serialize(Format format);
+
+	/**
+	 * 
+	 * @param format
+	 *            you want the Storable Object to be formatted to
+	 * @return the serialized version of the object
+	 */
+	abstract public String serialize( Format format );
+
 	
-	// Code review: space this out (I did it for you)
 	
+	
+	/**
+	 * @return a new Storage Key with an extension type of XML based on the Kind and
+	 *         ObjectId of the Storable object.
+	 */
 	public StorageKey createStorageKey()
 	{
-		return new StorageKey(getSimpleKind(), getSimpleObjectId(), "xml"); // CODE REVEIW: The third paramater should be StorageKeyExtension, not a string. Since it will be used so much, I suggest creating a static private final StorageKeyExtension XML = new StorageKeyExtension("xml"); in StorageKeyExtension
+		return new StorageKey(getSimpleKind(), getSimpleObjectId(), StorageKeyExtension.XML);
 	}
 }
--- aws/src/main/java/org/jimmutable/storage/StorageKeyExtension.java
+++ aws/src/main/java/org/jimmutable/storage/StorageKeyExtension.java
@@ -2,67 +2,81 @@ package org.jimmutable.storage;
 
 import org.jimmutable.core.objects.Stringable;
 import org.jimmutable.core.utils.Validator;
+
 /**
  * 
- * @author andrew.towe
- *	This class exists to help us manage the extensions of each of our storable objects. 
- *	This class handles the mimetypes.
+ * @author andrew.towe This class exists to help us manage the extensions of
+ *         each of our storable objects. This class handles the mimetypes.
  */
 public class StorageKeyExtension extends Stringable
-{	
-	public StorageKeyExtension(String value)
+{
+	public static StorageKeyExtension XML = new StorageKeyExtension("xml");
+
+	public StorageKeyExtension( String value )
 	{
 		super(value);
 	}
+
 	/**
-	 * @return the mime type of this extension.  
+	 * @return the mime type of this extension.
 	 */
 
-	public String getSimpleMimeType() 
+	public String getSimpleMimeType()
 	{
-		switch(getSimpleValue()){
-		case "html": return "text/html";
-		case "htm": return "text/htm";
-		case "css": return "text/css";
-		case "js": return "application/js";
-		case "json": return "application/json";
-		case "xml": return "application/xml";
-		case "jpeg": return "image/jpeg";
-		case "jpg": return "image/jpg";
-		case "gif": return "image/gif";
-		case "png": return "image/png";
-		case "pdf": return "application/pdf";
-		case "xslx": return "application/xslx";
-		case "csv": return "text/csv";
-		case "txt": return "text/txt";
-		default: return "application/octet-stream";
+		switch ( getSimpleValue() )
+		{
+		case "html":
+			return "text/html";
+		case "htm":
+			return "text/htm";
+		case "css":
+			return "text/css";
+		case "js":
+			return "application/js";
+		case "json":
+			return "application/json";
+		case "xml":
+			return "application/xml";
+		case "jpeg":
+			return "image/jpeg";
+		case "jpg":
+			return "image/jpg";
+		case "gif":
+			return "image/gif";
+		case "png":
+			return "image/png";
+		case "pdf":
+			return "application/pdf";
+		case "xslx":
+			return "application/xslx";
+		case "csv":
+			return "text/csv";
+		case "txt":
+			return "text/txt";
+		default:
+			return "application/octet-stream";
 		}
 	}
-	
+
 	@Override
-	public void normalize() 
+	public void normalize()
 	{
 		Validator.notNull(getSimpleValue());
-		
-		// CODE REVIEW: You can not do this becuase validate has *not* guaranteed that value is null
-		// TRY it out: StorageKeyExtension(null) will throw an NPE here
-		// I have fixed it for you, just remember this
-		
-		if( getSimpleValue() != null )
+
+		if ( getSimpleValue() != null )
 		{
-			// we want to strip out any leading ".". so that we do not confuse ourselves. 
-			
-			while( getSimpleValue().startsWith(".") )
+			while ( getSimpleValue().startsWith(".") )
 				setValue(getSimpleValue().substring(1));
 		}
 		normalizeLowerCase();
 	}
 
 	@Override
-	public void validate() 
+	public void validate()
 	{
-		Validator.min(getSimpleValue().length(), 1);//if we strip out the "." and nothing is left over, it will be caught here. 
-		Validator.containsOnlyValidCharacters(getSimpleValue(),Validator.LOWERCASE_LETTERS,Validator.NUMBERS);		
+		Validator.min(getSimpleValue().length(), 1);// if we strip out the "." and nothing is left over, it will be
+													// caught here.
+		Validator.containsOnlyValidCharacters(getSimpleValue(), Validator.LOWERCASE_LETTERS, Validator.NUMBERS);
 	}
 
 }
