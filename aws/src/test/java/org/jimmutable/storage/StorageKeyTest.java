a11182489b7a12e6e3f405b39529e59e46e4911f
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-01 12:39:00 -0700

    PNDA-154 storage key changes after code commit.

--- aws/src/test/java/org/jimmutable/storage/StorageKeyTest.java
+++ aws/src/test/java/org/jimmutable/storage/StorageKeyTest.java
@@ -3,34 +3,67 @@ package org.jimmutable.storage;
 import junit.framework.TestCase;
 import org.jimmutable.core.objects.common.Kind;
 import org.jimmutable.core.objects.common.ObjectId;
-public class StorageKeyTest extends TestCase 
+
+public class StorageKeyTest extends TestCase
 {
 	public void testParsing()
 	{
 		StorageKey key = new StorageKey("alpha/1234.txt");
-		assertEquals("alpha",key.getSimpleKind().getSimpleValue());
-		assert(key.getSimpleId().getSimpleValue().contains("1234"));
-		assertEquals("text/txt",key.getSimpleExtension().getSimpleMimeType());
+		assertEquals("alpha", key.getSimpleKind().getSimpleValue());
+		assert (key.getSimpleObjectId().getSimpleValue().contains("1234"));
+		assertEquals("text/txt", key.getSimpleExtension().getSimpleMimeType());
 	}
+
 	public void testFancyParsing()
 	{
 		Kind kind = new Kind("alpha");
 		ObjectId objectId = new ObjectId("1234");
-		String extension = "txt";
-		StorageKey key = new StorageKey(kind, objectId,extension);
-		assertEquals("alpha",key.getSimpleKind().getSimpleValue());
-		assert(key.getSimpleId().getSimpleValue().contains("1234"));
-		assertEquals("text/txt",key.getSimpleExtension().getSimpleMimeType());
+		StorageKeyExtension extension = new StorageKeyExtension("txt");
+		StorageKey key = new StorageKey(kind, objectId, extension);
+		assertEquals("alpha", key.getSimpleKind().getSimpleValue());
+		assert (key.getSimpleObjectId().getSimpleValue().contains("1234"));
+		assertEquals("text/txt", key.getSimpleExtension().getSimpleMimeType());
 	}
+
+	public void testBadParsing()
+	{
+		boolean fail = true;
+		try
+		{
+			StorageKey key = new StorageKey("foo");
+		} catch ( org.jimmutable.core.exceptions.ValidationException e )
+		{
+			fail = false;
+		} catch ( Exception e )
+		{
+			System.out.println();
+		}
+		assertFalse(fail);
+		fail = true;
+		try
+		{
+			StorageKey key = new StorageKey("foo/bar");
+		} catch ( org.jimmutable.core.exceptions.ValidationException e )
+		{
+			fail = false;
+		} catch ( Exception e )
+		{
+			System.out.println();
+		}
+		assertFalse(fail);
+	}
+
 	public void testNullParsing()
 	{
 		boolean fail = true;
 		try
 		{
 			StorageKey key = new StorageKey(null);
-		}catch(org.jimmutable.core.exceptions.ValidationException e) {
+		} catch ( org.jimmutable.core.exceptions.ValidationException e )
+		{
 			fail = false;
-		}catch (Exception e) {
+		} catch ( Exception e )
+		{
 			System.out.println();
 		}
 		assertFalse(fail);
@@ -38,9 +71,10 @@ public class StorageKeyTest extends TestCase
 		try
 		{
 			ObjectId objectId = new ObjectId("1234");
-			String extension = "txt";
-			StorageKey key = new StorageKey(null,objectId,extension);
-		}catch(org.jimmutable.core.exceptions.ValidationException e) {
+			StorageKeyExtension extension = new StorageKeyExtension("txt");
+			StorageKey key = new StorageKey(null, objectId, extension);
+		} catch ( org.jimmutable.core.exceptions.ValidationException e )
+		{
 			fail = false;
 		}
 		assertFalse(fail);
@@ -48,9 +82,10 @@ public class StorageKeyTest extends TestCase
 		try
 		{
 			Kind kind = new Kind("alpha");
-			String extension = "txt";
-			StorageKey key = new StorageKey(kind,null,extension);
-		}catch(org.jimmutable.core.exceptions.ValidationException e) {
+			StorageKeyExtension extension = new StorageKeyExtension("txt");
+			StorageKey key = new StorageKey(kind, null, extension);
+		} catch ( org.jimmutable.core.exceptions.ValidationException e )
+		{
 			fail = false;
 		}
 		assertFalse(fail);
@@ -59,9 +94,9 @@ public class StorageKeyTest extends TestCase
 		{
 			Kind kind = new Kind("alpha");
 			ObjectId objectId = new ObjectId("1234");
-			String extension = "txt";
-			StorageKey key = new StorageKey(kind,objectId,null);
-		}catch(org.jimmutable.core.exceptions.ValidationException e) {
+			StorageKey key = new StorageKey(kind, objectId, null);
+		} catch ( org.jimmutable.core.exceptions.ValidationException e )
+		{
 			fail = false;
 		}
 		assertFalse(fail);
