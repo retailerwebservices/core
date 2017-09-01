50abc9662b1e697fba992fc5b5e0090cd054314e
James H. Kane Jr <jim.kane@gmail.com>
2017-09-01 07:47:48 -0700

    Code review

--- aws/src/main/java/org/jimmutable/storage/Storage.java
+++ aws/src/main/java/org/jimmutable/storage/Storage.java
@@ -10,6 +10,7 @@ import org.jimmutable.core.objects.common.Kind;
  * It is designed to handle all updates, insertions, checking of existence, listing of objects, and deletions.
  */
 
+// CODE REVIEW:  Fix your spacing.  { on newline, and it if ( instance == null ) etc., newlines between methods etc.
 
 public abstract class Storage {
 
@@ -18,6 +19,8 @@ public abstract class Storage {
 	public Storage(boolean is_readOnly) {
 		this.is_readonly=is_readOnly;
 	}
+	
+	// CODE REVIEW: this method definitely needs javadoc
 	public Storage getSimpleInstance()
 	{
 		if(instance ==null)
@@ -34,15 +37,18 @@ public abstract class Storage {
 		return instance;
 	}
 
+	// CODE REVEIW: All of these methods need javadoc
 	public abstract boolean exists(StorageKey key, boolean default_value);
 	public abstract boolean upsert(StorageKey key, byte bytes[], boolean hint_content_likely_to_be_compressible);
 	public abstract byte[] getCurrentVersion(StorageKey key, byte[] default_value);
 	public abstract boolean delete(StorageKey key);
+	
+	// CODE REVEIW: Change the signature to listComplex and take a default return value (my mistake)
 	public abstract Iterable<StorageKey> list(Kind kind);
 
 	public boolean upsert(Storable obj)
 	{
-		Validator.notNull(obj);
+		Validator.notNull(obj); 
 		return upsert(obj.createStorageKey(),null,true);
 	}
 	public boolean exists(Storable obj, boolean default_value)
