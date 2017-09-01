35a18fc3ac7301b5a78fc530376bec67a4d295b5
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-01 16:38:35 -0700

    PNDA-191 formatting fix

--- aws/src/test/java/jimmutable_aws/messaging/common_messages/SubscriptionDefinitionTest.java
+++ aws/src/test/java/jimmutable_aws/messaging/common_messages/SubscriptionDefinitionTest.java
@@ -6,10 +6,12 @@ import junit.framework.TestCase;
 
 public class SubscriptionDefinitionTest extends TestCase
 {
-	
-	public static void testCreation() 
+
+	public static void testCreation()
 	{
-		SubscriptionDefinition subdef= new SubscriptionDefinition(new TopicDefinition(new ApplicationId("Development1"), new TopicId("flight_speed_of_a_swallow")),new QueueDefinition(new ApplicationId("Development2"),new QueueId("123456")));
+		SubscriptionDefinition subdef = new SubscriptionDefinition(
+				new TopicDefinition(new ApplicationId("Development1"), new TopicId("flight_speed_of_a_swallow")),
+				new QueueDefinition(new ApplicationId("Development2"), new QueueId("123456")));
 		assertEquals("development1/flight_speed_of_a_swallow/development2/123456", subdef.getSimpleValue());
 	}
 }
