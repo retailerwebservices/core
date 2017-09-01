ffac9de7615b7ca0d90ee893872b2e67e69ee467
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-01 13:13:51 -0700

    PNDA-186 created Topic Definition class and added javadoc to TopicId

---
+++ aws/src/main/java/jimmutable_aws/messaging/common_messages/TopicDefinition.java
@@ -0,0 +1,41 @@
+package jimmutable_aws.messaging.common_messages;
+
+import org.jimmutable.core.objects.Stringable;
+import org.jimmutable.core.utils.Validator;
+import org.jimmutable.storage.ApplicationId;
+/**
+ * 
+ * @author andrew.towe
+ * this class is designed to help us Define our Topics. 
+ */
+public class TopicDefinition extends Stringable
+{
+
+	public TopicDefinition( String value )
+	{
+		super(value);
+	}
+
+	public TopicDefinition( ApplicationId application_id, TopicId topic_id )
+	{
+		this(createStringFromComponents(application_id, topic_id));
+	}
+
+	private static String createStringFromComponents( ApplicationId application_id, TopicId topic_id )
+	{
+		return application_id.getSimpleValue() + "/" + topic_id.getSimpleValue();
+	}
+
+	@Override
+	public void normalize()
+	{
+		normalizeTrim();
+	}
+
+	@Override
+	public void validate()
+	{
+		Validator.notNull(getSimpleValue());
+	}
+
+}
--- aws/src/main/java/jimmutable_aws/messaging/common_messages/TopicId.java
+++ aws/src/main/java/jimmutable_aws/messaging/common_messages/TopicId.java
@@ -2,11 +2,15 @@ package jimmutable_aws.messaging.common_messages;
 
 import org.jimmutable.core.objects.Stringable;
 import org.jimmutable.core.utils.Validator;
-
+/**
+ * 
+ * @author andrew.towe
+ *This class is designed to help us monitor our messaging topics
+ */
 public class TopicId extends Stringable
 {
-	public static TopicId application_public= new TopicId("public");
-	public static TopicId application_private= new TopicId("private");
+	public static TopicId application_public = new TopicId("public");
+	public static TopicId application_private = new TopicId("private");
 
 	public TopicId( String value )
 	{
@@ -14,19 +18,20 @@ public class TopicId extends Stringable
 	}
 
 	@Override
-	public void normalize() 
+	public void normalize()
 	{
 		normalizeTrim();
 		normalizeLowerCase();
 	}
 
 	@Override
-	public void validate() 
+	public void validate()
 	{
 		Validator.notNull(getSimpleValue());
 		Validator.min(getSimpleValue().length(), 3);
 		Validator.max(getSimpleValue().length(), 64);
-		Validator.containsOnlyValidCharacters(getSimpleValue(), Validator.UNDERSCORE,Validator.LOWERCASE_LETTERS,Validator.NUMBERS);
+		Validator.containsOnlyValidCharacters(getSimpleValue(), Validator.UNDERSCORE, Validator.LOWERCASE_LETTERS,
+				Validator.NUMBERS);
 
 	}
 
