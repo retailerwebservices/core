9c8ba3d64613b45188d73f98906aeebaec0a1bd6
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-04 11:24:47 -0700

    PNDA-194 Corrected Javadoc

--- aws/src/main/java/jimmutable_aws/messaging/common_messages/MessageListener.java
+++ aws/src/main/java/jimmutable_aws/messaging/common_messages/MessageListener.java
@@ -2,15 +2,16 @@ package jimmutable_aws.messaging.common_messages;
 
 import org.jimmutable.core.objects.StandardObject;
 /**
- * 
+ * Implementing this interface to review messages from Messaging 
  * @author andrew.towe
- * This class exists so that we can listen for incoming messages.
  */
-
-// CODE REVEIW: For interfaces, javadoc would typically read something like this: "Implementing this interface to review messages from Messaging"
 
 public interface MessageListener
 {
-	// Code review: Javadoc needed on this method.  You need to let the user know (a) that message will never be null (b) that this is not executed in a different thread, so one should not consumer tons of time, etc.
+	/**
+	 * This is a lightweight method. Any implementation of this method should not need to take a lot of time
+	 * Should not be executed on a separate thread. 
+	 * @param message will never be null;
+	 */
 	public void onMessageReceived( StandardObject message );
 }
