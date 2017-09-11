0159e3d5b5c5ff27aaa9464533a9898c5236ee69
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-11 12:23:19 -0700

    Fixing my own branch.

---
+++ aws/src/main/java/org/jimmutable/storage/Storage.java
@@ -0,0 +1,118 @@
+package org.jimmutable.storage;
+
+import org.jimmutable.core.utils.Validator;
+import org.jimmutable.core.objects.common.Kind;
+
+/**
+ *
+ * @author andrew.towe This class is the parent of our file storage handlers. It
+ *         is designed to handle all updates, insertions, checking of existence,
+ *         listing of objects, and deletions.
+ */
+
+
+public abstract class Storage
+{
+
+	Storage instance = null;
+	private boolean is_readonly = false;
+
+	public Storage( boolean is_readOnly )
+	{
+		this.is_readonly = is_readOnly;
+	}
+
+	/**
+	 * @return either an instance of StorageDevLocalFileSystem if on a development application, otherwise will return a StorageGoogleCloudStorage
+	 */
+	public Storage getSimpleInstance()
+	{
+		if ( instance == null )
+		{
+			if ( ApplicationId.hasOptionalDevApplicationId() )
+			{
+				instance = new StorageDevLocalFileSystem(isReadOnly());
+			} else
+			{
+//				instance = new StorageGoogleCloudStorage(isReadOnly());
+			}
+		}
+		return instance;
+	}
+
+	/**
+	 * @param key of the Storable object that we are looking for. 
+	 * @param default_value to be returned if object is not found
+	 * @return true if object is found, else Default_value
+	 */
+	
+	public abstract boolean exists( StorageKey key, boolean default_value );
+
+	/**
+	 * @param key of the Storable Object to Update/Insert
+	 * @param bytes the contents of the Storable Object
+	 * @param hint_content_likely_to_be_compressible
+	 * @return true if the Object was updated/inserted, else false
+	 */
+	
+	public abstract boolean upsert( StorageKey key, byte bytes[], boolean hint_content_likely_to_be_compressible );
+
+	/**
+	 * @param key key associated with Stored Object you want to retrieve the current version of. 
+	 * @param default_value If the object is not found, what would you like returned. 
+	 * @return Byte array of Stored object if Object was found, otherwise default_value
+	 */
+	
+	public abstract byte[] getCurrentVersion( StorageKey key, byte[] default_value );
+	
+	/**
+	 * @param key StorageKey associated with StorageObject
+	 * @return true if Storage Object existed and was deleted, false otherwise
+	 */
+	public abstract boolean delete( StorageKey key );
+	
+	/**
+	 * @param kind The kind of the storable object you are looking for
+	 * @param default_value the value you want returned if nothing is found. 
+	 * @return If any StorageKeys were found, that Collection of objects will be returned, Otherwise the Default_value that was passed in. 
+	 */
+
+	public abstract Iterable<StorageKey> listComplex( Kind kind, Iterable<StorageKey> default_value);
+
+	public boolean upsert( Storable obj )
+	{
+		Validator.notNull(obj);
+		return upsert(obj.createStorageKey(), null, true);
+	}
+
+	public boolean exists( Storable obj, boolean default_value )
+	{
+		if ( obj == null )
+			return default_value;
+
+		try
+		{
+			return exists(obj.createStorageKey(), false);
+		} catch ( Exception e )
+		{
+			// LogManager.getRootLogger().error("Failure to list object "+obj,e);
+			return default_value;
+		}
+	}
+
+	public boolean delete( Storable obj )
+	{
+		if ( obj == null )
+			return false;
+		if ( isReadOnly() )
+			return false;
+
+		return delete(obj.createStorageKey());
+	}
+
+	public boolean isReadOnly()
+	{
+		return is_readonly;
+	}
+
+}
--- aws/src/main/java/org/jimmutable/storage/StorageKey.java
+++ aws/src/main/java/org/jimmutable/storage/StorageKey.java
@@ -9,137 +9,111 @@ import org.jimmutable.core.utils.Validator;
 
 /**
  * 
- * @author andrew.towe
- *	This class is designed to help us with how we store objects. 
- *	All storage objects will need a storage key. 
- *	Keys have three parts to them: the Kind, the Object Id, and the Extension of the storage object.
- *	
- */
-
-/**
- * CODE REVIEW: 
+ * @author andrew.towe This class is designed to help us with how we store
+ *         objects. All storage objects will need a storage key. Keys have three
+ *         parts to them: the Kind, the Object Id, and the Extension of the
+ *         storage object.
  * 
- * Fix your spacing, { go on newlines,
- * @author kanej
- *
  */
 
-public class StorageKey extends Stringable{
+public class StorageKey extends Stringable
+{
 	Kind kind;
 	ObjectId id;
 	StorageKeyExtension extension;
 
-<<<<<<< HEAD
 	/**
 	 * @param value
 	 *            a new Storage Key based on the string that is passed in. Therefore
 	 *            if the String passed in is "Alpha/123.txt" then the Kind will be
 	 *            "alpha" the Object Id will be "123" the extension will be "txt"
-=======
-	/** 
-	 * @param 
-	 * Creates a new Storage Key based on the string that is passed in.
-	 * Therefore if the String passed in is "Alpha/123.txt"
-	 * then
-	 * 		the Kind will be "alph"
-	 * 		the Object Id will be "123"
-	 * 		the extension will be "txt"
->>>>>>> origin/dev
 	 */
-	public StorageKey(String value) 
+	public StorageKey( String value )
 	{
 		super(value);
 	}
-	
+
 	/**
 	 * @param kind
-<<<<<<< HEAD
 	 *            the Kind used for the StorageKey
 	 * @param object_id
 	 *            the ObjectId used for the StorageKey
-=======
-	 * 		the Kind used for the StorageKey
-	 * @param objectId
-	 * 		the ObjectId used for the StorageKey
->>>>>>> origin/dev
 	 * @param extension
-	 * 		the extension of the StorageKey
+	 *            the extension of the StorageKey
 	 */
-	
-	// CODE REVIEW: Our field names are lower case with words separated by _
-	// The extension should be be a StorageKeyExtension object
-	
-	public StorageKey(Kind kind, ObjectId object_id, StorageKeyExtension extension) 
-	{	
+
+	public StorageKey( Kind kind, ObjectId object_id, StorageKeyExtension extension )
+	{
 		this(createStringFromComponents(kind, object_id, extension));
 	}
+
 	/**
 	 * 
-	 * /**
 	 * @param kind
-<<<<<<< HEAD
 	 *            the Kind used for the StorageKey
 	 * @param object_id
 	 *            the ObjectId used for the StorageKey
-=======
-	 * 		the Kind used for the StorageKey
-	 * @param objectId
-	 * 		the ObjectId used for the StorageKey
->>>>>>> origin/dev
 	 * @param extension
-	 * 		the extension of the StorageKey
+	 *            the extension of the StorageKey
 	 *
-	 * @return if Everything validates it will return a string that concatenates all of the parameters simple values. {alpha,123,"txt"}->"alpha/123.txt"
+	 * @return if Everything validates it will return a string that concatenates all
+	 *         of the parameters simple values. {alpha,123,"txt"}->"alpha/123.txt"
 	 */
-	
-	// CODE REVIEW: Make your method names indicative of what the function does
-	// I fixed your spacing for you
-	// static goes before private
-	
-	static private String createStringFromComponents(Kind kind, ObjectId object_id, StorageKeyExtension extension) 
+
+	static private String createStringFromComponents( Kind kind, ObjectId object_id, StorageKeyExtension extension )
 	{
-		Validator.notNull(kind,object_id,extension);
+		Validator.notNull(kind, object_id, extension);
 		return String.format("%s/%s.%s", kind.getSimpleValue(), object_id.getSimpleValue(), extension);
 	}
 
 	@Override
-	public void normalize() 
+	public void normalize()
 	{
 		normalizeLowerCase();
 	}
 
 	@Override
-	public void validate() 
+	public void validate()
 	{
-		// CODE REVIEW: Space your statements apart
-		// CODE REVIEW: You need to check the return of split.  What if the string does not have a / in it?  Also, your unit test should test things like new StorageKey("foo") which would show this problem...
-		// CODE REVIEW: You can't use File.separator ... on windows the seperator is "\" which will break everything.  Hard code "/" here
-		
+
 		Validator.notNull(getSimpleValue());
-		
-		String[] breakonslash = getSimpleValue().split(File.separator);
+
+		String[] breakonslash = getSimpleValue().split("/");
+		Validator.min(breakonslash.length, 2);
 		kind = new Kind(breakonslash[0]);
-		
+
 		String[] breakondot = breakonslash[1].split("\\.");
+		Validator.min(breakonslash.length, 2);
 		id = new ObjectId(breakondot[0]);
-		
-		extension= new StorageKeyExtension(breakondot[1]);
-		
-		// CODE REVIEW: at the end here you need to SET the value.  So something like setValue(createStringFromComponents(getSimpelKind(), getSimpleId(), getSimpleExtension());
-		
+
+		extension = new StorageKeyExtension(breakondot[1]);
+
+		setValue(createStringFromComponents(getSimpleKind(), getSimpleObjectId(),getSimpleExtension()));
+
 	}
+
 	/**
 	 * @return The Kind associated with the storage Key
 	 */
-	 public Kind getSimpleKind() { return kind; }
+	public Kind getSimpleKind()
+	{
+		return kind;
+	}
+
 	/**
 	 * @return The ObjectId associated with the storage Key
 	 */
-	 // CODE REVIEW: The method name should be getSimpleObjectId()
-	 public ObjectId getSimpleId() { return id; }
+	public ObjectId getSimpleObjectId()
+	{
+		return id;
+	}
+
 	/**
 	 * @return The Extension associated with the storage Key
 	 */
-	 public StorageKeyExtension getSimpleExtension() { return extension; }
+	public StorageKeyExtension getSimpleExtension()
+	{
+		return extension;
+	}
 
 }
