01f5631d2a814c47c5bedcaa0cb46f7fc4693be4
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-14 10:35:19 -0700

    PNDA-359 removed deprecated_old files

--- cloud/deprecated_old_src/src/simple_object_store/SimpleObjectStoreScanListener.java
+++
@@ -1,8 +0,0 @@
-package org.jimmutable.aws.simple_object_store;
-
-import org.jimmutable.aws.s3.S3Path;
-
-public interface SimpleObjectStoreScanListener 
-{
-	public void onS3ObjectScanned(S3Path path);
-}
