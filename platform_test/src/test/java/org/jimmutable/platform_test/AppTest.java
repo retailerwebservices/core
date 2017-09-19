b140af3ee2917bff3aa427c4e686f465f230aa50
Preston McCumber <preston.mccumber@digitalpanda.com>
2017-09-19 11:24:53 -0700

    Update to platform_test test class

--- platform_test/src/test/java/org/jimmutable/platform_test/AppTest.java
+++ platform_test/src/test/java/org/jimmutable/platform_test/AppTest.java
@@ -1,43 +1,22 @@
 package org.jimmutable.platform_test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import org.junit.Test;
+
+public class AppTest extends App
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+//	@Test
+	public void testMain()
+	{
+		try{
+			App.main(null);
+			assertTrue( true );
+		} catch (Exception e) {
+			e.printStackTrace();
+			assert(false);
+		}
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-    		try{
-    			App.main(null);
-    			assertTrue( true );
-    		} catch (Exception e) {
-    			assert(false);
-    		}
-    }
 }
