444cfb0f5eaf7a5cc54bbb8ebbd95529e7a88989
Andrew Towe <andrew.towe@digitalpanda.com>
2017-09-27 10:33:25 -0700

    PNDA-462 Created OpenGraphObjectId class.

---
+++ core/src/main/java/org/jimmutable/core/objects/common/OpenGraphObjectId.java
@@ -0,0 +1,49 @@
+package org.jimmutable.core.objects.common;
+
+import org.jimmutable.core.objects.Stringable;
+import org.jimmutable.core.serialization.reader.ObjectParseTree;
+import org.jimmutable.core.utils.Validator;
+
+public class OpenGraphObjectId extends Stringable
+{
+	public OpenGraphObjectId( String s )
+	{
+		super(s);
+	}
+
+	
+	public OpenGraphObjectId( ObjectParseTree reader )
+	{
+		super(reader);
+		
+	}
+
+	@Override
+	public void normalize()
+	{
+		
+	}
+
+	@Override
+	public void validate()
+	{
+		Validator.notNull(getSimpleValue());
+		Validator.min(getSimpleValue().length(), 1);
+		Validator.max(getSimpleValue().length(), 128);
+		Validator.containsOnlyValidCharacters(getSimpleValue(), Validator.NUMBERS);
+	}
+	static public class MyConverter extends Stringable.Converter<OpenGraphObjectId>
+	{
+		public OpenGraphObjectId fromString(String str, OpenGraphObjectId default_value)
+		{
+			try
+			{
+				return new OpenGraphObjectId(str);
+			}
+			catch(Exception e)
+			{
+				return default_value;
+			}
+		}
+	}
+}
