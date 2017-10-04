2d658a68a6a89f27a105d5ffc158a3875c632e1c
Andrew Towe <andrew.towe@digitalpanda.com>
2017-10-04 10:57:08 -0700

    PNDA-490 created AdvancedSearchComboBoxChoice class

---
+++ cloud/src/test/java/org/jimmutable/cloud/elasticsearch/AdvancedSearchComboBoxChoiceTest.java
@@ -0,0 +1,63 @@
+package org.jimmutable.cloud.elasticsearch;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.jimmutable.cloud.StubTest;
+import org.jimmutable.core.objects.StandardObject;
+import org.jimmutable.core.serialization.Format;
+import org.jimmutable.core.serialization.JimmutableTypeNameRegister;
+import org.jimmutable.core.serialization.reader.ObjectParseTree;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AdvancedSearchComboBoxChoiceTest extends StubTest
+{
+	@Before
+	public void before()
+	{
+
+		JimmutableTypeNameRegister.registerAllTypes();
+		ObjectParseTree.registerTypeName(SearchIndexDefinition.class);
+		ObjectParseTree.registerTypeName(SearchIndexFieldDefinition.class);
+		ObjectParseTree.registerTypeName(IncludeFieldInView.class);
+		ObjectParseTree.registerTypeName(AdvancedSearchComboBoxChoice.class);
+
+	}
+
+	@Test
+	public void testUserSerialization()
+	{
+		AdvancedSearchComboBoxChoice field = new AdvancedSearchComboBoxChoice("Name", "Jim");
+		String serialized_value = field.serialize(Format.JSON_PRETTY_PRINT);
+
+		System.out.println(field.toJavaCode(Format.JSON_PRETTY_PRINT, "obj"));
+		assertEquals("{\n" + "  \"type_hint\" : \"advancedsearchcomboboxchoice\",\n" + "  \"label\" : \"Name\",\n" + "  \"value\" : \"Jim\"\n" + "}", serialized_value);
+
+		String obj_string = String.format("%s\n%s\n%s\n%s\n%s", "{", "  \"type_hint\" : \"advancedsearchcomboboxchoice\",", "  \"label\" : \"Name\",", "  \"value\" : \"Jim\"", "}");
+
+		AdvancedSearchComboBoxChoice obj = (AdvancedSearchComboBoxChoice) StandardObject.deserialize(obj_string);
+
+		assertEquals("Name", obj.getSimpleLabel());
+	}
+
+	@Test
+	public void testUserComparisonAndEquals()
+	{
+		AdvancedSearchComboBoxChoice field = new AdvancedSearchComboBoxChoice("Name", "Andrew");
+		AdvancedSearchComboBoxChoice field_modified = new AdvancedSearchComboBoxChoice("Name", "Andrew");
+
+		assertTrue(field.equals(field_modified));
+		assertEquals(0, field.compareTo(field_modified));
+
+		field_modified = new AdvancedSearchComboBoxChoice("Name", "Trevor");
+		assertFalse(field.equals(field_modified));
+		assertEquals(-1,field.compareTo(field_modified));
+
+		field_modified = new AdvancedSearchComboBoxChoice("Name", "Aaron");
+		assertFalse(field.equals(field_modified));
+		assertEquals(1,field.compareTo(field_modified));
+	}
+
+}
