fbea33cb52766b8d9eba47f874c20754afce9347
James H. Kane Jr <jim.kane@gmail.com>
2017-10-10 08:23:06 -0700

    Improvements to FileUtils; MessageListenerDaemon and runnables

--- core/src/main/java/org/jimmutable/core/utils/FileUtils.java
+++ core/src/main/java/org/jimmutable/core/utils/FileUtils.java
@@ -2,10 +2,18 @@ package org.jimmutable.core.utils;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jimmutable.core.objects.StandardObject;
+import org.jimmutable.core.serialization.Format;
 
 public class FileUtils 
 {
+	private static final Logger logger = Logger.getLogger(FileUtils.class.getName()); 
+	
 	/**
 	 * Get the current user's home directory
 	 * 
@@ -54,6 +62,93 @@ public class FileUtils
 		{
 			e.printStackTrace();
 			return false;
+		}
+	}
+	
+	/**
+	 * Write a string to a file
+	 * 
+	 * @param dest
+	 *            The file to write to
+	 * @param value
+	 *            The string to write
+	 */
+	static public void writeFile(File dest, String value) throws IOException
+	{
+		Validator.notNull(dest, value);
+
+
+		FileWriter w = new FileWriter(dest);
+		w.write(value);
+		w.close();
+	}
+	
+	/**
+	 * Serialize and object to a file in the specified format
+	 * 
+	 * @param dest
+	 *            The file to write
+	 * @param object
+	 *            The object to write
+	 * @param format
+	 *            The format to serialize in
+	 * @throws IOException
+	 */
+	static public void writeFile(File dest, StandardObject object, Format format) throws IOException
+	{
+		Validator.notNull(dest, object, format);
+		
+		writeFile(dest, object.serialize(format));
+	}
+	
+	/**
+	 * Quiet (no chance of throwing an exception) write of an object to a file
+	 * 
+	 * @param dest
+	 *            The file to write
+	 * @param object
+	 *            The object to write
+	 * @param format
+	 *            The format to write the object in
+	 * @return true if the object was written successfully, false otherwise
+	 */
+	static public boolean quietWriteFile(File dest, StandardObject object, Format format)
+	{
+		try
+		{
+			writeFile(dest,object,format);
+			return true;
+		}
+		catch(Exception e)
+		{
+			logger.log(Level.WARNING, "error writing file", e);
+			return false;
+		}
+	}
+	
+	/**
+	 * Attempts to read a StandardObject from a file
+	 * 
+	 * @param src
+	 *            The file to read from
+	 * @param default_value
+	 *            The value to return if the read fails (for any reason)
+	 * @return The deserialzied object from the file, or default_value if the object
+	 *         could not be read
+	 */
+	static public StandardObject readObjectFromFile(File src, StandardObject default_value)
+	{
+		try
+		{
+			String str = getComplexFileContentsAsString(src, null);
+			if ( str == null ) return default_value;
+			
+			return StandardObject.deserialize(str);
+		}
+		catch(Exception e)
+		{
+			logger.log(Level.WARNING, "error reading object from file", e);
+			return default_value;
 		}
 	}
 	
