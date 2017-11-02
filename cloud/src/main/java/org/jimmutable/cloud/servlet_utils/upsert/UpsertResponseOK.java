739f839d7e49496900dd8b17fbbf4491390e5349
Trevor Box <trevor.box@digitalpanda.com>
2017-11-02 14:25:08 -0700

    changes...

--- cloud/src/main/java/org/jimmutable/cloud/servlet_utils/upsert/UpsertResponseOK.java
+++ cloud/src/main/java/org/jimmutable/cloud/servlet_utils/upsert/UpsertResponseOK.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import org.jimmutable.cloud.servlet_utils.common_objects.JSONServletResponse;
 import org.jimmutable.core.objects.StandardImmutableObject;
+import org.jimmutable.core.objects.Stringable;
 import org.jimmutable.core.serialization.FieldDefinition;
 import org.jimmutable.core.serialization.TypeName;
 import org.jimmutable.core.serialization.reader.ObjectParseTree;
@@ -12,24 +13,21 @@ import org.jimmutable.core.utils.Comparison;
 import org.jimmutable.core.utils.Optional;
 
 /**
- * UpsertResponseOK
- * Used to indicate successful response to update/insert request
+ * UpsertResponseOK Used to indicate successful response to update/insert
+ * request
  * 
- * @author Preston McCumber
- * Sep 1, 2017
+ * @author Preston McCumber Sep 1, 2017
  */
 public class UpsertResponseOK extends JSONServletResponse
 {
-	static public final TypeName TYPE_NAME = new TypeName(
-			"jimmutable.aws.servlet_utils.upsert.UpsertResponseOK");
+	static public final TypeName TYPE_NAME = new TypeName("jimmutable.aws.servlet_utils.upsert.UpsertResponseOK");
 
 	public TypeName getTypeName()
 	{
 		return TYPE_NAME;
 	}
 
-	static public final FieldDefinition.StandardObject FIELD_OBJECT = new FieldDefinition.StandardObject("object",
-			null);
+	static public final FieldDefinition.StandardObject FIELD_OBJECT = new FieldDefinition.StandardObject("object", null);
 	static public final FieldDefinition.String FIELD_MESSAGE = new FieldDefinition.String("message", null);
 
 	static public final int HTTP_STATUS_CODE_OK = 200;
@@ -41,23 +39,23 @@ public class UpsertResponseOK extends JSONServletResponse
 	{
 	};
 
-	public UpsertResponseOK( String message, StandardImmutableObject object )
+	public UpsertResponseOK(String message, StandardImmutableObject object)
 	{
 		this.message = message;
 		this.object = object;
 		complete();
 	}
 
-	public UpsertResponseOK( ObjectParseTree t )
+	public UpsertResponseOK(ObjectParseTree t)
 	{
 		this.message = t.getString(FIELD_MESSAGE);
 		this.object = (StandardImmutableObject) t.getObject(FIELD_OBJECT);
 	}
 
 	@Override
-	public int compareTo( JSONServletResponse obj )
+	public int compareTo(JSONServletResponse obj)
 	{
-		if ( !(obj instanceof UpsertResponseOK) )
+		if (!(obj instanceof UpsertResponseOK))
 			return 0;
 
 		UpsertResponseOK other = (UpsertResponseOK) obj;
@@ -68,10 +66,20 @@ public class UpsertResponseOK extends JSONServletResponse
 	}
 
 	@Override
-	public void write( ObjectWriter writer )
+	public void write(ObjectWriter writer)
 	{
 		writer.writeString(FIELD_MESSAGE, getOptionalMessage(null));
-		writer.writeObject(FIELD_OBJECT, getOptionalObject(null));
+
+		if (getOptionalObject(null) instanceof Stringable)
+		{
+
+			Stringable s = (Stringable) getOptionalObject(null);
+			writer.writeStringable(FIELD_OBJECT, s);
+		} else
+		{
+
+			writer.writeObject(FIELD_OBJECT, getOptionalObject(null));
+		}
 	}
 
 	@Override
@@ -80,12 +88,12 @@ public class UpsertResponseOK extends JSONServletResponse
 		return HTTP_STATUS_CODE_OK;
 	}
 
-	public String getOptionalMessage( String default_value )
+	public String getOptionalMessage(String default_value)
 	{
 		return Optional.getOptional(message, null, default_value);
 	}
 
-	public StandardImmutableObject getOptionalObject( String default_value )
+	public StandardImmutableObject getOptionalObject(String default_value)
 	{
 		return (StandardImmutableObject) Optional.getOptional(object, null, default_value);
 	}
@@ -112,15 +120,15 @@ public class UpsertResponseOK extends JSONServletResponse
 	}
 
 	@Override
-	public boolean equals( Object obj )
+	public boolean equals(Object obj)
 	{
-		if ( !(obj instanceof UpsertResponseOK) )
+		if (!(obj instanceof UpsertResponseOK))
 			return false;
 
 		UpsertResponseOK other = (UpsertResponseOK) obj;
-		if ( !Objects.equals(getOptionalMessage(null), other.getOptionalMessage(null)) )
+		if (!Objects.equals(getOptionalMessage(null), other.getOptionalMessage(null)))
 			return false;
-		if ( !Objects.equals(getOptionalObject(null), other.getOptionalObject(null)) )
+		if (!Objects.equals(getOptionalObject(null), other.getOptionalObject(null)))
 			return false;
 
 		return true;
