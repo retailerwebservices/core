470ad35688bef6d6e37ea153c23622f415446c58
Avery Gonzales <avery.gonzales@retailerwebservices.com>
2018-04-19 10:15:13 -0700

    Roll back to reaper of indices concept for alias update

--- cloud/src/main/java/org/jimmutable/cloud/elasticsearch/ElasticSearch.java
+++ cloud/src/main/java/org/jimmutable/cloud/elasticsearch/ElasticSearch.java
@@ -1113,27 +1113,33 @@ public class ElasticSearch implements ISearch
 
 		Set<String> old_index = getCurrentIndiciesFromAliasName(definition.getSimpleIndex().getSimpleValue());
 		String[] indices = old_index.toArray(new String[old_index.size()]);
-		String alias = definition.getSimpleIndex().getSimpleValue();
+
+		// Has to be atomic so if it fails we don't add a new alias
 		try
 		{
-			// Has to be atomic so if it fails we don't add a new alias
-			IndicesAliasesRequestBuilder builder = client.admin().indices().prepareAliases().addAlias(index_name, alias);
-			for (String index : indices)
-			{
-				builder.removeIndex(index);
-			}
-			IndicesAliasesResponse response = builder.execute().actionGet();
+			IndicesAliasesResponse response = client.admin().indices().prepareAliases().addAlias(index_name, definition.getSimpleIndex().getSimpleValue()).removeAlias(indices, definition.getSimpleIndex().getSimpleValue()).execute().actionGet();
 			if (!response.isAcknowledged())
 			{
 				logger.fatal(String.format("Alias addition not acknowledged for index %s", index_name));
 				return false;
 			}
-		}
-		catch (Exception e)
+		} catch (Exception e)
 		{
 			logger.error("Alias addition and removal failed", e);
 			return false;
-		}	
+		}
+		
+		// Once we have confirmed the swap we no longer need the indices that we removed
+		// from the alias so we will delete them all
+		// This operation could be handled by a reaper thread in the future if we want
+		// to keep the job of deleting indices outside of the reindexer
+		for (String index : indices)
+		{
+			if (!deleteIndex(index))
+			{
+				logger.error("Left over index " + index + " removal failed, continuing so others can be attempted to remove.");
+			}
+		}
 
 		return true;
 	}
