e5a304c15d3156fd53233e4af6c5178ce3c93111
Trevor Box <trevor.box@digitalpanda.com>
2018-04-19 11:42:37 -0700

    fixed a bug within StandardImmutableObjectCache

--- cloud/src/main/java/org/jimmutable/cloud/storage/StandardImmutableObjectCache.java
+++ cloud/src/main/java/org/jimmutable/cloud/storage/StandardImmutableObjectCache.java
@@ -2,6 +2,8 @@ package org.jimmutable.cloud.storage;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jimmutable.cloud.CloudExecutionEnvironment;
 import org.jimmutable.cloud.messaging.signal.SignalTopicId;
 import org.jimmutable.core.objects.StandardImmutableObject;
@@ -16,26 +18,31 @@ public class StandardImmutableObjectCache
 {
 	private ExpirationCache<ObjectReference, StandardImmutableObject> cache = new ExpirationCache<>(TimeUnit.MINUTES.toMillis(5), 100_000);
 
+	private static final Logger logger = LogManager.getLogger(StandardImmutableObjectCache.class);
+
 	/**
-	 * Central topic for all standard immutable objects that are going to be upserted 
+	 * Central topic for all standard immutable objects that are going to be
+	 * upserted
 	 */
 	public static SignalTopicId TOPIC_ID = new SignalTopicId("standard-immutable-object-cache");
-	
+
 	public static void setupListeners()
 	{
 		UpsertListener upsert_listener = new UpsertListener();
 		CloudExecutionEnvironment.getSimpleCurrent().getSimpleSignalService().startListening(TOPIC_ID, upsert_listener);
 	}
-	
-	public void put( Kind kind, ObjectId id, StandardImmutableObject object )
+
+	public void put(Kind kind, ObjectId id, StandardImmutableObject object)
 	{
-		if(kind==null||id==null||object==null) return;
+		if (kind == null || id == null || object == null)
+			return;
 		put(new ObjectReference(kind, id), object);
 	}
 
-	public void put( ObjectReference object_reference, StandardImmutableObject object )
+	public void put(ObjectReference object_reference, StandardImmutableObject object)
 	{
-		if ( object_reference == null || object == null ) return;
+		if (object_reference == null || object == null)
+			return;
 		cache.put(object_reference, object);
 	}
 
@@ -52,37 +59,41 @@ public class StandardImmutableObjectCache
 	 *
 	 * - JMD
 	 * 
-	 * Response
-	 * For you to get anything out of the cache, one needs a kind and an ObjectId. 
-	 * This is a requirement of ObjectReference, which we are using as the key in the cache. 
-	 * If a class does not have these methods the developer will have to supply them (or make them up).
-	 * No, not everything in the cache must be a Storable, but being a Storable makes things easier.  
-	 * This is simply a way to save someone a little typing. 
+	 * Response For you to get anything out of the cache, one needs a kind and an
+	 * ObjectId. This is a requirement of ObjectReference, which we are using as the
+	 * key in the cache. If a class does not have these methods the developer will
+	 * have to supply them (or make them up). No, not everything in the cache must
+	 * be a Storable, but being a Storable makes things easier. This is simply a way
+	 * to save someone a little typing.
 	 * 
 	 */
-	public <T extends Storable> boolean has( T object )
+	public <T extends Storable> boolean has(T object)
 	{
-		if(object==null)return false;
+		if (object == null)
+			return false;
 		return has(new ObjectReference(object.getSimpleKind(), object.getSimpleObjectId()));
 	}
 
-	public boolean has( Kind kind, ObjectId id )
+	public boolean has(Kind kind, ObjectId id)
 	{
-		if(kind==null||id==null)return false;
+		if (kind == null || id == null)
+			return false;
 		return has(new ObjectReference(kind, id));
 	}
 
-	public boolean has( ObjectReference object_reference )
+	public boolean has(ObjectReference object_reference)
 	{
-		if(object_reference==null) {
+		if (object_reference == null)
+		{
 			return false;
 		}
 		return cache.has(object_reference);
 	}
 
-	public StandardImmutableObject get( Kind kind, ObjectId id, StandardImmutableObject default_value )
+	public StandardImmutableObject get(Kind kind, ObjectId id, StandardImmutableObject default_value)
 	{
-		if(kind==null||id==null) {
+		if (kind == null || id == null)
+		{
 			return default_value;
 		}
 		return get(new ObjectReference(kind, id), default_value);
@@ -92,33 +103,59 @@ public class StandardImmutableObjectCache
 	// the idea of getComplex? (because this (and the overload) would be a complex
 	// method) - JMD
 	// Response
-	// Yes we do, but we use those methods for getting complex things from a StandardImmutableObject specifically
-	// the reason I called this method get is because when we want to acquire something from a map or other storage 
-	// places, we use the method signature get. I am trying to stay consistent with that methodology. 
-	public StandardImmutableObject get( ObjectReference reference, StandardImmutableObject default_value )
-	{		
-		StandardImmutableObject standard_immutable_object =  cache.getOptional(reference, default_value);
-		if(default_value!=null&&standard_immutable_object.equals(default_value)) return default_value;
-		
-		//if you did not find it in the cache go find it in storage. 
-		byte[] object = CloudExecutionEnvironment.getSimpleCurrent().getSimpleStorage().getCurrentVersion(new ObjectIdStorageKey(reference.getSimpleKind(), reference.getSimpleObjectId(), StorageKeyExtension.JSON), new byte[0]);
-
-		standard_immutable_object = (StandardImmutableObject) ObjectParseTree.deserialize(new String(object));
-		if ( standard_immutable_object != null )
+	// Yes we do, but we use those methods for getting complex things from a
+	// StandardImmutableObject specifically
+	// the reason I called this method get is because when we want to acquire
+	// something from a map or other storage
+	// places, we use the method signature get. I am trying to stay consistent with
+	// that methodology.
+	public StandardImmutableObject get(ObjectReference reference, StandardImmutableObject default_value)
+	{
+		if (reference == null)
 		{
-			//if there is something, cache it and return it. 
+			return default_value;
+		}
+
+		StandardImmutableObject standard_immutable_object = cache.getOptional(reference, default_value);
+		if (default_value != null && standard_immutable_object.equals(default_value))
+		{
+			return default_value;
+		}
+
+		// if you did not find it in the cache go find it in storage.
+
+		byte[] bytes = CloudExecutionEnvironment.getSimpleCurrent().getSimpleStorage().getCurrentVersion(new ObjectIdStorageKey(reference.getSimpleKind(), reference.getSimpleObjectId(), StorageKeyExtension.JSON), null);
+
+		if (bytes == null)
+		{
+			logger.error(String.format("Failed to retreive %s from storage!", reference.getSimpleValue()));
+			return default_value;
+		}
+
+		try
+		{
+			standard_immutable_object = (StandardImmutableObject) ObjectParseTree.deserialize(new String(bytes));
+		} catch (Exception e)
+		{
+
+			logger.error(String.format("Failed to serialize %s!", reference.getSimpleValue()), e);
+		}
+
+		if (standard_immutable_object != null)
+		{
+			// if there is something, cache it and return it.
 			cache.put(reference, standard_immutable_object);
 			return standard_immutable_object;
 		}
-		//else return default_value. 
-		return  default_value;
+		// else return default_value.
+		return default_value;
 	}
 
-	public void remove( ObjectReference reference )
+	public void remove(ObjectReference reference)
 	{
 		cache.remove(reference);
 	}
-	
-	//we use this class in CloudExecutionEnvironment. 
-	
+
+	// we use this class in CloudExecutionEnvironment.
+
 }
