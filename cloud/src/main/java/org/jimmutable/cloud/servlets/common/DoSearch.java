3f3d88aa8e920b2c4bd6d29f80f807d7c3a0fb3b
Andrew Towe <andrew.towe@digitalpanda.com>
2019-01-23 12:46:15 -0700

    Test case fixes and better writing

--- cloud/src/main/java/org/jimmutable/cloud/servlets/common/DoSearch.java
+++ cloud/src/main/java/org/jimmutable/cloud/servlets/common/DoSearch.java
@@ -254,10 +254,14 @@ public abstract class DoSearch extends HttpServlet
 			StringJoiner refined_search_string = new StringJoiner(" AND");
 			for ( String clause : clauses )
 			{
+				boolean inclusive = false;
 				if ( clause.contains(">") || clause.contains("<") )
 				{ // if we need to do any faffing about with ranges.
 					String split_string = ":>";
-					clause= clause.replace("=", "");
+					if(clause.contains("=")) {
+						clause= clause.replace("=", "");
+						inclusive = true;
+					}
 					if ( clause.contains("<") )
 					{
 						split_string = ":<";
@@ -270,7 +274,11 @@ public abstract class DoSearch extends HttpServlet
 							date_string= date_string+" 0:00";
 						}
 						Date date = formatter.parse(date_string);
+						if(inclusive) {
 						refined_search_string.add("("+clause_breakdown[0] + split_string + date.getTime()+" OR "+clause_breakdown[0] + ":" + date.getTime()+")");
+						}else {
+							refined_search_string.add(clause_breakdown[0] + split_string + date.getTime());
+						}
 						
 					}
 					catch ( ParseException e )
