19e783222d079618e0bbd02bcea5ce4b1d47316d
Preston McCumber <preston.mccumber@retailerwebservices.com>
2023-08-25 13:43:06 -0700

    Update custom sort field mapping

--- cloud/src/main/java/org/jimmutable/cloud/elasticsearch/ElasticSearchCommon.java
+++ cloud/src/main/java/org/jimmutable/cloud/elasticsearch/ElasticSearchCommon.java
@@ -41,56 +41,74 @@ public class ElasticSearchCommon
 
 	public static final String ELASTICSEARCH_DEFAULT_TYPE = "mappings";
 	public static final String SORT_FIELD_NAME_JIMMUTABLE = "jimmutable_sort_field";
-	
+
 	public static final Set<SearchIndexFieldType> SEARCH_INDEX_FIELD_TYPES_REQUIRING_SORT_FIELDS = Sets.newHashSet(SearchIndexFieldType.TEXT, SearchIndexFieldType.INSTANT, SearchIndexFieldType.TIMEOFDAY);
 
-	public static XContentBuilder getMappingBuilder( SearchIndexDefinition index, XContentBuilder default_value )
+	public static XContentBuilder getMappingBuilder(SearchIndexDefinition index, XContentBuilder default_value)
 	{
 		XContentBuilder mappingBuilder;
 		try
 		{
 			mappingBuilder = jsonBuilder();
 			mappingBuilder.startObject().startObject("properties");
-			for ( SearchIndexFieldDefinition field : index.getSimpleFields() )
+			for (SearchIndexFieldDefinition field : index.getSimpleFields())
 			{
 				mappingBuilder.startObject(field.getSimpleFieldName().getSimpleName());
 				{
 					mappingBuilder.field("type", field.getSimpleType().getSimpleSearchType());
 				}
 				mappingBuilder.endObject();
-				if ( SEARCH_INDEX_FIELD_TYPES_REQUIRING_SORT_FIELDS.contains(field.getSimpleType()) )
+
+				if (SEARCH_INDEX_FIELD_TYPES_REQUIRING_SORT_FIELDS.contains(field.getSimpleType()))
 				{
-					mappingBuilder.startObject(determineSortFieldName(field));
-					{
-						mappingBuilder.field("type", field.getSimpleType().getSimpleSearchType());
-						mappingBuilder.startObject("fields");
-						{
-							mappingBuilder.startObject("keyword");
-							{
-								mappingBuilder.field("type", "keyword");
-								mappingBuilder.field("ignore_above", 256);
-							}
-							mappingBuilder.endObject();
-						}
-						mappingBuilder.endObject();
-					}
-					mappingBuilder.endObject();
+					mapCustomSortField(mappingBuilder, field);
 				}
 			}
 			mappingBuilder.endObject().endObject();
 			mappingBuilder.close();
 			return mappingBuilder;
 		}
-		catch ( Exception e )
+		catch (Exception e)
 		{
 			logger.error(String.format("Failed to generate mapping json for index %s", index.getSimpleIndex().getSimpleValue()), e);
 			return default_value;
 		}
 	}
-	
+
+	private static void mapCustomSortField(XContentBuilder mappingBuilder, SearchIndexFieldDefinition field) throws Exception
+	{
+		mappingBuilder.startObject(determineSortFieldName(field));
+		
+		if ( field.getSimpleType() == SearchIndexFieldType.TEXT )
+		{
+			mappingBuilder.field("type", "text");
+			mapCustomSortFieldText(mappingBuilder, field);
+		}
+		else if ( field.getSimpleType() == SearchIndexFieldType.INSTANT || field.getSimpleType() == SearchIndexFieldType.TIMEOFDAY)
+		{
+			mappingBuilder.field("type", "long");
+		}
+
+		mappingBuilder.endObject();
+	}
+
+	private static void mapCustomSortFieldText(XContentBuilder mappingBuilder, SearchIndexFieldDefinition field) throws Exception
+	{
+		mappingBuilder.startObject("fields");
+		{
+			mappingBuilder.startObject("keyword");
+			{
+				mappingBuilder.field("type", "keyword");
+				mappingBuilder.field("ignore_above", 256);
+			}
+			mappingBuilder.endObject();
+		}
+		mappingBuilder.endObject();
+	}
+
 	private static String determineSortFieldName(SearchIndexFieldDefinition field) throws Exception
 	{
-		switch(field.getSimpleType())
+		switch (field.getSimpleType())
 		{
 		case TEXT:
 			return getSortFieldNameText(field.getSimpleFieldName());
@@ -103,10 +121,10 @@ public class ElasticSearchCommon
 		}
 	}
 
-	public static TypeMapping getMappingBuilderTypeMapping( SearchIndexDefinition index, TypeMapping default_value )
+	public static TypeMapping getMappingBuilderTypeMapping(SearchIndexDefinition index, TypeMapping default_value)
 	{
 		XContentBuilder content_builder = getMappingBuilder(index, null);
-		if ( content_builder == null )
+		if (content_builder == null)
 		{
 			return default_value;
 		}
@@ -115,10 +133,10 @@ public class ElasticSearchCommon
 
 	}
 
-	public static SourceField getMappingBuilderSourceField( SearchIndexDefinition index, SourceField default_value )
+	public static SourceField getMappingBuilderSourceField(SearchIndexDefinition index, SourceField default_value)
 	{
 		XContentBuilder content_builder = getMappingBuilder(index, null);
-		if ( content_builder == null )
+		if (content_builder == null)
 		{
 			return default_value;
 		}
@@ -127,7 +145,7 @@ public class ElasticSearchCommon
 
 	}
 
-	protected static boolean shutdownDocumentUpsertThreadPool( int timeout_seconds )
+	protected static boolean shutdownDocumentUpsertThreadPool(int timeout_seconds)
 	{
 		long start = System.currentTimeMillis();
 
@@ -139,12 +157,12 @@ public class ElasticSearchCommon
 		{
 			terminated = document_upsert_pool.awaitTermination(timeout_seconds, TimeUnit.SECONDS);
 		}
-		catch ( InterruptedException e )
+		catch (InterruptedException e)
 		{
 			logger.error("Shutdown of runnable pool was interrupted!", e);
 		}
 
-		if ( !terminated )
+		if (!terminated)
 		{
 			logger.error(String.format("Failed to terminate in %s seconds. Calling shutdownNow...", timeout_seconds));
 			document_upsert_pool.shutdownNow();
@@ -152,53 +170,47 @@ public class ElasticSearchCommon
 
 		boolean success = document_upsert_pool.isTerminated();
 
-		if ( success )
+		if (success)
 		{
-			logger.warn(String.format("Successfully terminated pool in %s milliseconds", (System.currentTimeMillis()
-					- start)));
+			logger.warn(String.format("Successfully terminated pool in %s milliseconds", (System.currentTimeMillis() - start)));
 		}
 		else
 		{
-			logger.warn(String.format("Unsuccessful termination of pool in %s milliseconds", (System.currentTimeMillis()
-					- start)));
+			logger.warn(String.format("Unsuccessful termination of pool in %s milliseconds", (System.currentTimeMillis() - start)));
 		}
 		return success;
 	}
 
 	/**
-	 * Using a SortBy object, construct a SortBuilder used by ElasticSearch. This
-	 * method handles the unique sorting cases for Text, Time of Day, and Instant.
+	 * Using a SortBy object, construct a SortBuilder used by ElasticSearch.
+	 * This method handles the unique sorting cases for Text, Time of Day, and
+	 * Instant.
 	 * 
 	 * @param sort_by
 	 * @param default_value
 	 * @return
 	 */
-	static public FieldSortBuilder getSort( SortBy sort_by, FieldSortBuilder default_value )
+	static public FieldSortBuilder getSort(SortBy sort_by, FieldSortBuilder default_value)
 	{
 		SortOrder order = null;
-		if ( sort_by.getSimpleDirection() == SortDirection.ASCENDING )
-			order = SortOrder.ASC;
-		if ( sort_by.getSimpleDirection() == SortDirection.DESCENDING )
-			order = SortOrder.DESC;
+		if (sort_by.getSimpleDirection() == SortDirection.ASCENDING) order = SortOrder.ASC;
+		if (sort_by.getSimpleDirection() == SortDirection.DESCENDING) order = SortOrder.DESC;
 
-		if ( order == null )
-			return default_value;
+		if (order == null) return default_value;
 
 		FieldName field_name = sort_by.getSimpleField().getSimpleFieldName();
 		String sort_on_string = field_name.getSimpleName();
 
-		if ( sort_by.getSimpleField().getSimpleType() == SearchIndexFieldType.TEXT )
+		if (sort_by.getSimpleField().getSimpleType() == SearchIndexFieldType.TEXT)
 		{
 			// Reference getSortFieldNameText for logic on calling ATOM search type here
-			sort_on_string = getSortFieldNameText(sort_by.getSimpleField().getSimpleFieldName())
-					+ "."
-					+ SearchIndexFieldType.ATOM.getSimpleSearchType();
+			sort_on_string = getSortFieldNameText(sort_by.getSimpleField().getSimpleFieldName()) + "." + SearchIndexFieldType.ATOM.getSimpleSearchType();
 		}
-		if ( sort_by.getSimpleField().getSimpleType() == SearchIndexFieldType.TIMEOFDAY )
+		if (sort_by.getSimpleField().getSimpleType() == SearchIndexFieldType.TIMEOFDAY)
 		{
 			sort_on_string = getSortFieldNameTimeOfDay(sort_by.getSimpleField().getSimpleFieldName());
 		}
-		if ( sort_by.getSimpleField().getSimpleType() == SearchIndexFieldType.INSTANT )
+		if (sort_by.getSimpleField().getSimpleType() == SearchIndexFieldType.INSTANT)
 		{
 			sort_on_string = getSortFieldNameInstant(sort_by.getSimpleField().getSimpleFieldName());
 		}
@@ -208,9 +220,9 @@ public class ElasticSearchCommon
 
 	/**
 	 * Sorting on text fields is impossible without enabling fielddata in
-	 * ElasticSearch. To get around this, we instead use a keyword field for every
-	 * text field written. This solution is recommended by ElasticSearch over
-	 * enabling fielddata. For more information, read here:
+	 * ElasticSearch. To get around this, we instead use a keyword field for
+	 * every text field written. This solution is recommended by ElasticSearch
+	 * over enabling fielddata. For more information, read here:
 	 * 
 	 * https://www.elastic.co/guide/en/elasticsearch/reference/5.4/fielddata.html#before-enabling-fielddata
 	 * https://www.elastic.co/blog/support-in-the-wild-my-biggest-elasticsearch-problem-at-scale
@@ -218,16 +230,16 @@ public class ElasticSearchCommon
 	 * @param field
 	 * @return
 	 */
-	static public String getSortFieldNameText( FieldName field )
+	static public String getSortFieldNameText(FieldName field)
 	{
 		return getSortFieldNameText(field.getSimpleName());
 	}
 
 	/**
 	 * Sorting on text fields is impossible without enabling fielddata in
-	 * ElasticSearch. To get around this, we instead use a keyword field for every
-	 * text field written. This solution is recommended by ElasticSearch over
-	 * enabling fielddata. For more information, read here:
+	 * ElasticSearch. To get around this, we instead use a keyword field for
+	 * every text field written. This solution is recommended by ElasticSearch
+	 * over enabling fielddata. For more information, read here:
 	 * 
 	 * https://www.elastic.co/guide/en/elasticsearch/reference/5.4/fielddata.html#before-enabling-fielddata
 	 * https://www.elastic.co/blog/support-in-the-wild-my-biggest-elasticsearch-problem-at-scale
@@ -235,68 +247,60 @@ public class ElasticSearchCommon
 	 * @param field_name
 	 * @return
 	 */
-	static public String getSortFieldNameText( String field_name )
+	static public String getSortFieldNameText(String field_name)
 	{
-		return field_name
-				+ "_"
-				+ SORT_FIELD_NAME_JIMMUTABLE
-				+ "_"
-				+ SearchIndexFieldType.ATOM.getSimpleSearchType();
+		return field_name + "_" + SORT_FIELD_NAME_JIMMUTABLE + "_" + SearchIndexFieldType.ATOM.getSimpleSearchType();
 	}
 
 	/**
-	 * In order to sort TimeOfDay objects, we need to look at the ms_from_midnight
-	 * field from within. This method creates a consistent field name to sort by.
+	 * In order to sort TimeOfDay objects, we need to look at the
+	 * ms_from_midnight field from within. This method creates a consistent
+	 * field name to sort by.
 	 * 
 	 * @param field
 	 * @return
 	 */
-	static public String getSortFieldNameTimeOfDay( FieldName field )
+	static public String getSortFieldNameTimeOfDay(FieldName field)
 	{
 		return getSortFieldNameTimeOfDay(field.getSimpleName());
 	}
 
 	/**
-	 * In order to sort TimeOfDay objects, we need to look at the ms_from_midnight
-	 * field from within. This method creates a consistent field name to sort by.
+	 * In order to sort TimeOfDay objects, we need to look at the
+	 * ms_from_midnight field from within. This method creates a consistent
+	 * field name to sort by.
 	 * 
 	 * @param field_name
 	 * @return
 	 */
-	static public String getSortFieldNameTimeOfDay( String field_name )
+	static public String getSortFieldNameTimeOfDay(String field_name)
 	{
-		return field_name
-				+ "_"
-				+ SORT_FIELD_NAME_JIMMUTABLE
-				+ "_"
-				+ TimeOfDay.FIELD_MS_FROM_MIDNIGHT.getSimpleFieldName().getSimpleName();
+		return field_name + "_" + SORT_FIELD_NAME_JIMMUTABLE + "_" + TimeOfDay.FIELD_MS_FROM_MIDNIGHT.getSimpleFieldName().getSimpleName();
 	}
 
 	/**
-	 * In order to sort Instant objects, we need to look at the ms_from_epoch field
-	 * from within. This method creates a consistent field name to sort by.
+	 * In order to sort Instant objects, we need to look at the ms_from_epoch
+	 * field from within. This method creates a consistent field name to sort
+	 * by.
 	 * 
 	 * @param field
 	 * @return
 	 */
-	static public String getSortFieldNameInstant( FieldName field )
+	static public String getSortFieldNameInstant(FieldName field)
 	{
 		return getSortFieldNameInstant(field.getSimpleName());
 	}
 
 	/**
-	 * In order to sort Instant objects, we need to look at the ms_from_epoch field
-	 * from within. This method creates a consistent field name to sort by.
+	 * In order to sort Instant objects, we need to look at the ms_from_epoch
+	 * field from within. This method creates a consistent field name to sort
+	 * by.
 	 * 
 	 * @param field_name
 	 * @return
 	 */
-	static public String getSortFieldNameInstant( String field_name )
+	static public String getSortFieldNameInstant(String field_name)
 	{
-		return field_name
-				+ "_"
-				+ SORT_FIELD_NAME_JIMMUTABLE
-				+ "_"
-				+ Instant.FIELD_MS_FROM_EPOCH.getSimpleFieldName().getSimpleName();
+		return field_name + "_" + SORT_FIELD_NAME_JIMMUTABLE + "_" + Instant.FIELD_MS_FROM_EPOCH.getSimpleFieldName().getSimpleName();
 	}
 }
